eb6ae0398d72f77bbcf9f3013160dad026f4b198
frowing37 <froward37@gmail.com>
2024-01-24 04:17:50 +0300

    #2

--- SignalRWebUI/Models/Dto's/DiscountDto/CreateDiscountDto.cs
+++ SignalRWebUI/Models/Dto's/DiscountDto/CreateDiscountDto.cs
@@ -11,8 +11,10 @@ namespace SignalRWebUI.Models.Dtos.DiscountDto
         public string Amount { get; set; }
 
         public string Description { get; set; }
-
+        
         public string ImageURL { get; set; }
+        
+        public List<int> ProductIDs { get; set; }
         
     }
 }
